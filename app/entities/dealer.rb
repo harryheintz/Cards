19ed4080e476ac57a58b7106a119141ab9f9bc82
Harry Heintz <harryheintz@me.com>
2013-07-21 13:51:56 -0400

    Initial commit

--- app/entities/dealer.rb
+++ app/entities/dealer.rb
@@ -3,15 +3,22 @@ require_relative './card_deck'
 
 class Dealer < CardDeck
   
+  
   attr_accessor :cards
   
-  
-  
- def initialize(fresh_deck = Dealer.new(CardDeck.new.cards))
+  def initialize(fresh_deck)
     
-    deck = fresh_deck
+    @deck = fresh_deck
     
   end
+  
+  
+  
+  
+  
+  
+  fresh_deck = CardDeck.new.cards  
+ 
   
   
 end
