a3e77984566043eb0cb3b81127123cb433837ad5
Harry Heintz <harryheintz@me.com>
2013-08-04 15:27:45 -0400

    Added number of players and a "hand" method.

--- app/entities/blackjack.rb
+++ app/entities/blackjack.rb
@@ -1,3 +1,21 @@
-class Blackjack 
+class Blackjack
+  
+  attr_accessor :number_of_players, :player, :number_of_cards
+  
+  def initialize(individuals)
+    self.player = individuals
+    
+  end
+  
+  def number_of_players
+    self.player
+    
+  end
+  
+  def hand
+    number_of_cards = (number_of_players * 2)
+    
+  end
+ 
   
 end
--- spec/app/entities/blackjack_spec.rb
+++ spec/app/entities/blackjack_spec.rb
@@ -1,7 +1,26 @@
 require_relative '../spec_helper'
+require_relative '../../../app/entities/dealer'
+require_relative '../../../app/entities/card_deck'
 require_relative '../../../app/entities/blackjack'
 
 describe Blackjack do
+  
+  context "initialization" do
+    it "should return a number of players" do
+      result = Blackjack.new(1).number_of_players
+      expect(result).to be > 0 
+    end
+    
+  end
+  context "card distribution" do
+    
+    it "should return the correct number of card for the hand" do
+      result = Blackjack.new(2).hand
+      result_2 = Blackjack.new(2).number_of_players
+      expect(result / result_2).to equal 2
+    end
+  end
+  
   
 end
 
