e35f4b3ae342dc767e65288d2986e7519f7831e4
Harry Heintz <harryheintz@me.com>
2013-10-28 16:13:39 -0400

    The API mystery continues… pushed some changes to the start method. Some parsing problems with json now fixed. Still fails to run.

--- implementation/api/v1/api_implementation.rb
+++ implementation/api/v1/api_implementation.rb
@@ -8,11 +8,14 @@ module Implementation
       end
       
       put '/start' do
-        if game = BlackjackGame.start({user_id: 1, number_of_players: 3})
-          game.to_json
-        else
-          status 404
-        end
+        # if game = BlackjackGame.start({user_id: 1, number_of_players: 3})
+ #           game.to_json
+ #        else
+ #          status 404
+ #        end
+         attributes =  {number_of_players: 3, user_id: 1}.to_json
+         game = BlackjackGame.start(attributes) 
+         
       end
       
       post '/play' do
